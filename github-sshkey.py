068acc4241254db4a649a01d39fa6f5d4868862d
Cedric Creusot <cedric.creusot@gmail.com>
2020-08-18 10:07:50 +0200

    add option to use a pathphrase

--- github-sshkey.py
+++ github-sshkey.py
@@ -9,14 +9,16 @@ import shutil
 parser = argparse.ArgumentParser(description='Create a new key and add config')
 parser.add_argument("email", metavar="email", help="your user email")
 parser.add_argument("account_name", metavar="account_name", help="suffix of the id_rsa file")
+parser.add_argument("-p", dest="pathphrase", metavar="pathphrase", help="add a pathphrase", required=False)
 
 if __name__ == '__main__':
     args = parser.parse_args()
     filename = "id_rsa_" + args.account_name
+    pathphrase = "" if args.pathphrase == None else args.pathphrase 
     ssh_path = os.path.expanduser("~") + "/.ssh/"
 
-    # ssh-keygen -t rsa -P "" -C email -f filename
-    result = subprocess.run(['ssh-keygen', '-t', 'rsa', '-P', "", '-C', args.email, '-f', filename])
+    # ssh-keygen -t rsa -P "pathphrase" -C email -f filename
+    result = subprocess.run(['ssh-keygen', '-t', 'rsa', '-P', pathphrase, '-C', args.email, '-f', filename])
     result.check_returncode()
     
     shutil.move(filename, ssh_path)
